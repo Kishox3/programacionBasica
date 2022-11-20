8336acb72c9d8f6b7ce58c01b075ada4dce3c322
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-11-20 09:35:54 -0600

    remaquetado del codigo, seleccion de mascotas pc y pj, display dinamico de seleccion y botones de atk esp y fis

--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -1,7 +1,101 @@
-function seleccionarMokeponJugador()
+let ataqueJugador
+let ataqueComputadora
+
+function numeroAleatorio(min, max)
 {
-    alert("Seleccionaste tu Mokepon")
+    return Math.floor(Math.random()*(max - min + 1) + min)
 }
 
-let botonMokeponJugador = document.getElementById("boton-mokepon")
-botonMokeponJugador.addEventListener("click", seleccionarMokeponJugador) 
+
+function iniciarJuego()
+{
+    let botonMokeponJugador = document.getElementById("boton-seleccionar-mokepon")
+    botonMokeponJugador.addEventListener("click", flujoVideojuego)
+}
+
+function flujoVideojuego()
+{
+    seleccionarMokeponJugador()
+    seleccionarMokeponComputadora()
+    mostrarMokeponDinamicamente(seleccionarMokeponJugador(), seleccionarMokeponComputadora())
+
+    let botonAtaqueEspecial = document.getElementById("boton-ataque-especial")
+    botonAtaqueEspecial.addEventListener("click", ataqueEspecial)
+
+    let botonAtaqueFisico = document.getElementById("boton-ataque-fisico")
+    botonAtaqueFisico.addEventListener("click", ataqueFisico)
+}
+
+function seleccionarMokeponJugador()
+{
+    let mokepon001 = document.getElementById("mokepon001")
+    let mokepon002 = document.getElementById("mokepon002")
+    let mokepon003 = document.getElementById("mokepon003")
+    let mokeponJugador = ""
+
+    if(mokepon001.checked)
+    {
+        mokeponJugador = "Drauion 🔥"
+    }
+    else if(mokepon002.checked)
+    {
+        mokeponJugador = "Balisk 💧"
+    }
+    else if(mokepon003.checked)
+    {
+        mokeponJugador = "Imir 🌿"
+    }
+    else
+    {
+        alert("Debes seleccionar un Mokepon")
+    }
+
+    //alert("El Mokepon del jugador es: " + mokeponJugador)
+    return mokeponJugador
+}
+
+function seleccionarMokeponComputadora()
+{
+    let random = numeroAleatorio(1,3)
+    let mokeponComputadora = ""
+    
+    if(random == 1)
+    {
+        mokeponComputadora = "Drauion 🔥"
+    }
+    else if(random == 2)
+    {
+        mokeponComputadora = "Balisk 💧"
+    }
+    else if(random == 3)
+    {
+        mokeponComputadora = "Imir 🌿"
+    }
+    //alert("El Mokepon de la computadora es: " + mokeponComputadora)
+    return mokeponComputadora
+}
+
+function mostrarMokeponDinamicamente(jugador, computadora)
+{
+    let spanMokeponJugador = document.getElementById("span-mokepon-jugador")
+    let spanMokeponComputadora = document.getElementById("span-mokepon-computadora")
+
+    spanMokeponComputadora.innerHTML = computadora
+    spanMokeponJugador.innerHTML = jugador   
+}
+
+function ataqueEspecial()
+{
+    ataqueJugador = "ATK Especial"
+    alert(ataqueJugador)
+}
+
+function ataqueFisico()
+{
+    ataqueJugador = "ATK Fisico"
+    alert(ataqueJugador)
+}
+
+//escuchara los eventos despues de cargar todo el html
+window.addEventListener("load", iniciarJuego) 
+
